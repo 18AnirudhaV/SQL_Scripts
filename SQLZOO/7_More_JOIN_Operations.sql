0303bb110591d7ff5d0666e0d74b808012b65827
Anirudha Verma <yodatime29@gmail.com>
2023-01-27 16:03:06 -0500

    Update 7_More_JOIN_Operations.sql

--- SQLZOO/7_More_JOIN_Operations.sql
+++ SQLZOO/7_More_JOIN_Operations.sql
@@ -4,7 +4,7 @@ List the films where the yr is 1962 [Show id, title]
 */
 SELECT id, title
 FROM movie
-WHERE yr=1962
+WHERE yr = 1962
 
 # 2
 /*
@@ -44,7 +44,7 @@ WHERE title = 'Casablanca'
 Obtain the cast list for 'Casablanca'.
 */
 SELECT name 
-FROM movie JOIN casting on movie.id = movieid JOIN actor ON actor.id = actorid
+FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
 WHERE movieid = 27
 
 # 7
@@ -52,7 +52,7 @@ WHERE movieid = 27
 Obtain the cast list for the film 'Alien'
 */
 SELECT name 
-FROM movie JOIN casting on movie.id = movieid JOIN actor ON actor.id = actorid
+FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
 WHERE movie.title = 'Alien'
 
 # 8
@@ -60,22 +60,76 @@ WHERE movie.title = 'Alien'
 List the films in which 'Harrison Ford' has appeared
 */
 SELECT title
- FROM movie JOIN casting on movie.id = movieid 
-            JOIN actor ON actor.id = actorid
- WHERE actor.name = 'Harrison Ford'
+FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
+WHERE actor.name = 'Harrison Ford'
 
 # 9
 /*
 List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
 */
 SELECT title
-FROM movie JOIN casting on movie.id = movieid JOIN actor ON actor.id = actorid
-WHERE actor.name = 'Harrison Ford' AND casting.ord != 1
+FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
+WHERE actor.name = 'Harrison Ford'
+AND casting.ord != 1
 
 # 10
 /*
 List the films together with the leading star for all 1962 films.
 */
 SELECT title, actor.name
-FROM movie JOIN casting on movie.id = movieid JOIN actor ON actor.id = actorid
+FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
 WHERE casting.ord = 1 AND movie.yr = 1962
+
+# 11
+/*
+Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+*/
+SELECT yr, COUNT(title) as 'Movies Made'
+FROM movie JOIN casting ON movie.id=movieid JOIN actor ON actorid=actor.id
+WHERE actor.name = 'Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 1
+
+# 12
+/*
+List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+Did you get "Little Miss Marker twice"?
+Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
+
+Title is not a unique field, create a table of IDs in your subquery
+*/
+SELECT title, name 
+FROM movie JOIN casting x ON movie.id = movieid JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND 
+ movieid IN (SELECT movieid FROM casting y JOIN actor ON actor.id = actorid WHERE name='Julie Andrews')
+
+# 13
+/*
+Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+*/
+SELECT name
+FROM actor JOIN casting ON (id = actorid AND 
+ (SELECT COUNT(ord) FROM casting WHERE actorid = actor.id AND ord=1)>=15)
+GROUP BY name
+
+# 14
+/*
+List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+*/
+SELECT title, COUNT(actorid)
+FROM casting,movie                
+WHERE yr = 1978 AND movieid=movie.id
+GROUP BY title
+ORDER BY 2 DESC, 1 ASC
+
+# 15
+/*
+List all the people who have worked with 'Art Garfunkel'.
+*/
+SELECT DISTINCT d.name
+FROM actor d JOIN casting a ON (a.actorid=d.id)
+   JOIN casting b on (a.movieid=b.movieid)
+   JOIN actor c on (b.actorid=c.id 
+                and c.name='Art Garfunkel')
+WHERE d.id != c.id
